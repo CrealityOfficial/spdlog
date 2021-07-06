921e3f27eb9f18e189050111c405918ea2702bad
anoob <anoob@sina.cn>
2021-07-06 10:38:08 +0800

    spdlog

---
+++ src/include/spdlog/cxlog.h
@@ -0,0 +1,55 @@
+#ifndef _CXLOG_H
+#define _CXLOG_H
+#include<memory>
+#include<string>
+#include <spdlog/common.h>
+
+namespace spdlog
+{
+	class logger;
+}
+
+namespace cxlog
+{
+	class SPDLOG_API CXLog
+	{
+	public:
+		static CXLog& Instance();
+
+		void InitCXLog(std::string file_name="cxslice.log", int maxSize=1024*10, int maxFiles=4, int log_level=0);
+
+		void info(const char* fmt,...);
+		void error(const char* fmt,...);
+		void debug(const char* fmt, ...);
+		void warn(const char* fmt, ...);
+		void critical(const char* fmt, ...);
+
+		void EndLog();
+
+		void SetLevel(int level = 0);
+
+		auto GetLogger()
+		{
+			return mp_logger_;
+		}
+
+	protected:
+		CXLog() {};
+		~CXLog() {};
+		CXLog(const CXLog& other) = delete;
+		CXLog& operator=(const CXLog& other) = delete;
+	private:
+		std::shared_ptr<spdlog::logger> mp_logger_;
+		bool hasInitLog = false;
+	};
+}
+
+#define MAX_LOG_LEN 128
+#define CXLogInfo(...)  cxlog::CXLog::Instance().info(__VA_ARGS__)
+#define CXLogDebug(...)  cxlog::CXLog::Instance().debug(__VA_ARGS__)
+#define CXLogWarn(...) cxlog::CXLog::Instance().warn(__VA_ARGS__)
+#define CXLogError(...)  cxlog::CXLog::Instance().error(__VA_ARGS__)
+#define CXLogCritical(...)  cxlog::CXLog::Instance().critical(__VA_ARGS__)
+//#define CXLogIns
+
+#endif
---
+++ src/src/cxlog.cpp
@@ -0,0 +1,86 @@
+#include "spdlog/cxlog.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/async.h"
+#include <stdarg.h> 
+
+namespace cxlog {
+
+	CXLog& CXLog::Instance()
+	{
+		static CXLog cxlog;
+		return cxlog;
+	}
+	void CXLog::InitCXLog(std::string file_name, int maxSize, int maxFiles,int log_level)
+	{
+		if (hasInitLog)return;
+		//设置日志等级
+		spdlog::set_level(static_cast<spdlog::level::level_enum>(log_level));
+		//设置日志为滚动日志
+		mp_logger_ =spdlog::rotating_logger_mt("cxlog", file_name,maxSize,maxFiles);
+		//当遇到错误级别以上的马上刷新到日志
+		mp_logger_->flush_on(spdlog::level::info);
+		//每三秒刷新一次
+		spdlog::flush_every(std::chrono::seconds(3));
+		hasInitLog = true;
+	}
+
+	void CXLog::info(const char* fmt, ...)
+	{
+		//暂无法传递可变参数到spdloger,先解析完再传
+		char buf[MAX_LOG_LEN] = { 0 };
+		va_list args;
+		va_start(args, fmt);
+		vsprintf(buf, fmt, args);
+		mp_logger_->info(buf);
+		va_end(args);
+	}
+	void CXLog::error(const char* fmt, ...)
+	{
+		char buf[MAX_LOG_LEN] = { 0 };
+		va_list args;
+		va_start(args, fmt);
+		vsprintf(buf, fmt, args);
+		mp_logger_->error(buf);
+		va_end(args);
+	}
+	void CXLog::debug(const char* fmt, ...)
+	{
+		char buf[MAX_LOG_LEN] = { 0 };
+		va_list args;
+		va_start(args, fmt);
+		vsprintf(buf, fmt, args);
+		mp_logger_->debug(buf);
+		va_end(args);
+	}
+	void CXLog::warn(const char* fmt, ...)
+	{
+		char buf[MAX_LOG_LEN] = { 0 };
+		va_list args;
+		va_start(args, fmt);
+		vsprintf(buf, fmt, args);
+		mp_logger_->warn(buf);
+		va_end(args);
+	}
+
+	void CXLog::critical(const char* fmt, ...)
+	{
+		char buf[MAX_LOG_LEN] = { 0 };
+		va_list args;
+		va_start(args, fmt);
+		vsprintf(buf, fmt, args);
+		mp_logger_->critical(buf);
+		va_end(args);
+	}
+	void CXLog::EndLog()
+	{
+		spdlog::shutdown();
+	}
+	void CXLog::SetLevel(int level)
+	{
+		spdlog::set_level(static_cast<spdlog::level::level_enum>(level));
+	}
+}
+
+
+
