04f667a722a041172c633387a210e27c2d97b6a8
liuhong <78237139@qq.com>
2021-08-04 20:28:20 +0800

    modify bug
    Change-Id: I252eac7c2c4e29b73429e02083cdf58107344842

--- include/spdlog/cxlog_macro.h
+++ include/spdlog/cxlog_macro.h
@@ -4,19 +4,19 @@
 
 #define MAX_LOG_LEN 10240
 
-//#define CXLogInfo(...)  cxlog::CXLog::Instance().info(__VA_ARGS__)
+#define CXLogInfo(...)  cxlog::CXLog::Instance().info(__VA_ARGS__)
 #define CXLogInfo(logSortId, ...) cxlog::CXLog::Instance().info(logSortId, ##__VA_ARGS__)
 
-//#define CXLogDebug(...)  cxlog::CXLog::Instance().debug(__VA_ARGS__)
+#define CXLogDebug(...)  cxlog::CXLog::Instance().debug(__VA_ARGS__)
 #define CXLogDebug(logSortId, ...) cxlog::CXLog::Instance().debug(logSortId, ##__VA_ARGS__)
 
-//#define CXLogWarn(...) cxlog::CXLog::Instance().warn(__VA_ARGS__)
+#define CXLogWarn(...) cxlog::CXLog::Instance().warn(__VA_ARGS__)
 #define CXLogWarn(logSortId, ...) cxlog::CXLog::Instance().warn(logSortId, ##__VA_ARGS__)
 
-//#define CXLogError(...)  cxlog::CXLog::Instance().error(__VA_ARGS__)
+#define CXLogError(...)  cxlog::CXLog::Instance().error(__VA_ARGS__)
 #define CXLogError(logSortId, ...) cxlog::CXLog::Instance().error(logSortId, ##__VA_ARGS__)
 
-//#define CXLogCritical(...)  cxlog::CXLog::Instance().critical(__VA_ARGS__)
+#define CXLogCritical(...)  cxlog::CXLog::Instance().critical(__VA_ARGS__)
 #define CXLogCritical(logSortId,...)  cxlog::CXLog::Instance().critical(logSortId,##__VA_ARGS__)
 
 #endif // _CXLOG_MACRO_H
