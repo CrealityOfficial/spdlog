0dcab34f22d00543921c742bab319f6bd7a21c02
anoob <anoob@sina.cn>
2022-05-05 14:47:14 +0800

    macro

--- include/spdlog/cxlog_macro.h
+++ include/spdlog/cxlog_macro.h
@@ -14,23 +14,23 @@ enum class CXLOG_Level
 };
 
 #define CXLogVerbose(...) cxlog::CXLog::Instance().verbose(__VA_ARGS__)
-#define CXLogVerbose(logSortId, ...) cxlog::CXLog::Instance().verbose(logSortId, ##__VA_ARGS__)
+#define CXLogVerboseID(logSortId, ...) cxlog::CXLog::Instance().verbose(logSortId, ##__VA_ARGS__)
 
 #define CXLogDebug(...) cxlog::CXLog::Instance().debug(__VA_ARGS__)
-#define CXLogDebug(logSortId, ...) cxlog::CXLog::Instance().debug(logSortId, ##__VA_ARGS__)
+#define CXLogDebugID(logSortId, ...) cxlog::CXLog::Instance().debug(logSortId, ##__VA_ARGS__)
 
 #define CXLogInfo(...)  cxlog::CXLog::Instance().info(__VA_ARGS__)
-#define CXLogInfo(logSortId, ...) cxlog::CXLog::Instance().info(logSortId, ##__VA_ARGS__)
+#define CXLogInfoID(logSortId, ...) cxlog::CXLog::Instance().info(logSortId, ##__VA_ARGS__)
 
 #define CXLogWarn(...) cxlog::CXLog::Instance().warn(__VA_ARGS__)
-#define CXLogWarn(logSortId, ...) cxlog::CXLog::Instance().warn(logSortId, ##__VA_ARGS__)
+#define CXLogWarnID(logSortId, ...) cxlog::CXLog::Instance().warn(logSortId, ##__VA_ARGS__)
 
 #define CXLogError(...)  cxlog::CXLog::Instance().error(__VA_ARGS__)
-#define CXLogError(logSortId, ...) cxlog::CXLog::Instance().error(logSortId, ##__VA_ARGS__)
+#define CXLogErrorID(logSortId, ...) cxlog::CXLog::Instance().error(logSortId, ##__VA_ARGS__)
 
 #define CXLogCritical(...)  cxlog::CXLog::Instance().critical(__VA_ARGS__)
-#define CXLogCritical(logSortId,...)  cxlog::CXLog::Instance().critical(logSortId,##__VA_ARGS__)
+#define CXLogCriticalID(logSortId, ...) cxlog::CXLog::Instance().critical(logSortId, ##__VA_ARGS__)
 
 #define CXLogMain(...) cxlog::CXLog::Instance().main(__VA_ARGS__)
-#define CXLogMain(logSortId, ...) cxlog::CXLog::Instance().main(logSortId, ##__VA_ARGS__)
+#define CXLogMainID(logSortId, ...) cxlog::CXLog::Instance().main(logSortId, ##__VA_ARGS__)
 #endif // _CXLOG_MACRO_H
